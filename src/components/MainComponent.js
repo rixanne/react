c883805397ba6398ac67a9cec8860a6e49abd30a
rixanne <rixanne@users.noreply.github.com>
2021-07-01 00:01:27 -0400

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
@@ -20,6 +21,7 @@ class Main extends Component {
             comments: COMMENTS,
             partners: PARTNERS,
             promotions: PROMOTIONS
+            
         };
     }
 
@@ -52,6 +54,7 @@ class Main extends Component {
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
                     <Redirect to='/home' />
                 </Switch>
 
